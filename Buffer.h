7cd6f33804fb4f3bc5d75a6b68539de6cd0fccba
lymorass <lymorass@gmail.com>
2022-07-03 23:13:21 +0800

    add BUffer及相应问题

---
+++ Buffer.h
@@ -0,0 +1,201 @@
+//
+// Created by morass on 2022/7/3.
+//
+
+#ifndef WEBSERVER_BUFFER_H
+#define WEBSERVER_BUFFER_H
+
+#include "base/copyable.h"
+
+#include <algorithm>
+#include <vector>
+#include <assert.h>
+#include <string.h>
+
+
+/// A buffer class modeled after org.jboss.netty.buffer.ChannelBuffer
+///
+/// @code
+/// +-------------------+------------------+------------------+
+/// | prependable bytes |  readable bytes  |  writable bytes  |
+/// |                   |     (CONTENT)    |                  |
+/// +-------------------+------------------+------------------+
+/// |                   |                  |                  |
+/// 0      <=      readerIndex   <=   writerIndex    <=     size
+/// @endcode
+class Buffer : public copyable {
+public:
+    static const size_t kCheapPrepend = 8;
+    static const size_t kInitialSize = 1024;
+
+    explicit Buffer(size_t initialSize = kInitialSize)
+            : buffer_(kCheapPrepend + initialSize),
+              readerIndex_(kCheapPrepend),
+              writerIndex_(kCheapPrepend) {
+        assert(readableBytes() == 0);
+        assert(writableBytes() == initialSize);
+        assert(prependableBytes() == kCheapPrepend);
+    }
+
+
+    void swap(Buffer &rhs) {
+        buffer_.swap(rhs.buffer_);
+        std::swap(readerIndex_, rhs.readerIndex_);
+        std::swap(writerIndex_, rhs.writerIndex_);
+    }
+
+    size_t readableBytes() const { return writerIndex_ - readerIndex_; }
+
+    size_t writableBytes() const { return buffer_.size() - writerIndex_; }
+
+    size_t prependableBytes() const { return readerIndex_; }
+
+    const char *peek() const { return begin() + readerIndex_; }
+
+    const char *findCRLF() const {
+        const char *crlf = std::search(peek(), beginWrite(), kCRLF, kCRLF + 2);
+        return crlf == beginWrite() ? NULL : crlf;
+    }
+
+    const char *findCRLF(const char *start) const {
+        assert(peek() <= start);
+        assert(start <= beginWrite());
+        const char *crlf = std::search(start, beginWrite(), kCRLF, kCRLF + 2);
+        return crlf == beginWrite() ? NULL : crlf;
+    }
+
+    const char *findEOL() const {
+        const void *eol = memchr(peek(), '\n', readableBytes());
+        return static_cast<const char *>(eol);
+    }
+
+    const char *findEOL(const char *start) const {
+        assert(peek() <= start);
+        assert(start <= beginWrite());
+        const void *eol = memchr(start, '\n', beginWrite() - start);
+        return static_cast<const char *>(eol);
+    }
+
+    // retrieve returns void, to prevent
+    // string str(retrieve(readableBytes()), readableBytes());
+    // the evaluation of two functions are unspecified
+    void retrieve(size_t len) {
+        assert(len <= readableBytes());
+        if (len < readableBytes()) {
+            readerIndex_ += len;
+        } else {
+            retrieveAll();
+        }
+    }
+
+    void retrieveUntil(const char *end) {
+        assert(peek() <= end);
+        assert(end <= beginWrite());
+        retrieve(end - peek());
+    }
+
+    void retrieveAll()   //复原
+    {
+        readerIndex_ = kCheapPrepend;
+        writerIndex_ = kCheapPrepend;
+        buffer_.resize(kInitialSize + kCheapPrepend);
+    }
+    void retrieveAllFake()   //虚假复原
+    {
+        readerIndex_ = kCheapPrepend;
+        writerIndex_ = kCheapPrepend;
+    }
+
+    std::string retrieveAllAsString()
+    {
+        return retrieveAsString(readableBytes());
+    }
+
+    std::string retrieveAsString(size_t len)
+    {
+        assert(len <= readableBytes());
+        std::string result(peek(), len);
+        retrieve(len);
+        return result;
+    }
+
+    void append(const char * /*restrict*/ data, size_t len) {
+        ensureWritableBytes(len);
+        std::copy(data, data + len, beginWrite());
+        hasWritten(len);
+    }
+
+    void append(const void * /*restrict*/ data, size_t len) {
+        append(static_cast<const char *>(data), len);
+    }
+
+    void ensureWritableBytes(size_t len) {
+        if (writableBytes() < len) {
+            makeSpace(len);
+        }
+        assert(writableBytes() >= len);
+    }
+
+    char *beginWrite() { return begin() + writerIndex_; }
+
+    const char *beginWrite() const { return begin() + writerIndex_; }
+
+    void hasWritten(size_t len) {
+        assert(len <= writableBytes());
+        writerIndex_ += len;
+    }
+
+    void unwrite(size_t len) {
+        assert(len <= readableBytes());
+        writerIndex_ -= len;
+    }
+
+    void prepend(const void * /*restrict*/ data, size_t len) {
+        assert(len <= prependableBytes());
+        readerIndex_ -= len;
+        const char *d = static_cast<const char *>(data);
+        std::copy(d, d + len, begin() + readerIndex_);
+    }
+
+
+    size_t internalCapacity() const {
+        return buffer_.capacity();
+    }
+
+    /// Read data directly into buffer.
+    ///
+    /// It may implement with readv(2)
+    /// @return result of read(2), @c errno is saved
+    ssize_t readFd(int fd, int *savedErrno);
+
+private:
+
+    char *begin() { return &*buffer_.begin(); }
+
+    const char *begin() const { return &*buffer_.begin(); }
+
+    void makeSpace(size_t len) {
+        if (writableBytes() + prependableBytes() < len + kCheapPrepend) {
+            buffer_.resize(writerIndex_ + len);
+        } else {
+            // move readable data to the front, make space inside buffer
+            assert(kCheapPrepend < readerIndex_);
+            size_t readable = readableBytes();
+            std::copy(begin() + readerIndex_,
+                      begin() + writerIndex_,
+                      begin() + kCheapPrepend);
+            readerIndex_ = kCheapPrepend;
+            writerIndex_ = readerIndex_ + readable;
+            assert(readable == readableBytes());
+        }
+    }
+
+private:
+    std::vector<char> buffer_;
+    size_t readerIndex_;
+    size_t writerIndex_;
+
+    static const char kCRLF[];
+};
+
+#endif
